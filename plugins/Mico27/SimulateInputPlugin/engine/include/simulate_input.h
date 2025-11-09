61a5b458eab1cb5b3f043b0e96027a8e2f79d5f2
akira <akira@akira.local>
2025-11-09 15:45:59 -0500

    finally

--- plugins/Mico27/SimulateInputPlugin/engine/include/simulate_input.h
+++
@@ -1,9 +0,0 @@
-#ifndef SIMULATE_INPUT_H
-#define SIMULATE_INPUT_H
-
-#include <gbdk/platform.h>
-
-void simulate_input_init(UBYTE preserve) BANKED;
-void simulate_input_update(void) BANKED;
-
-#endif
